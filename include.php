67405649242f2c32b9b2442521b216aef17705cf
sunkang <13127955213@qq.com>
2023-11-16 14:34:31 +0800

    new version

--- include.php
+++ include.php
@@ -1,10 +1,4 @@
 <?php 
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware
-    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com
-*/
 /**
 *  RPC 服务 
 */ 
--- service/sso.php
+++ service/sso.php
@@ -1,11 +1,4 @@
 <?php 
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware
-    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com
-*/
-
 /**
 * 获取登录后的信息
 */
